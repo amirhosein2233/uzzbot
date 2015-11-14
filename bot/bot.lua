3362ad1d51ff9b59b22c1522801abcb0e27c3269
amirhosein2233 <amirhosein2233@mail.com>
2015-11-14 06:38:25 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {our_id},
+    sudo_users = {129070606,129646018},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
